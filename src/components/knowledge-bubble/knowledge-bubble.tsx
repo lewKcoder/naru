2be9651b2d8b453bbf573a8b9fad0d5702d13621
lewKcoder <7kamimura74@gmail.com>
2024-05-17 23:02:12 +0900

    feat: :sparkles: Added sparkle elements and styles

--- src/components/knowledge-bubble/knowledge-bubble.tsx
+++ src/components/knowledge-bubble/knowledge-bubble.tsx
@@ -1,4 +1,5 @@
 import { Component } from "./types";
+import styles from "./styles.module.scss";
 
 export const KnowledgeBubble: Component = () => {
   const absoluteCenter =
@@ -6,15 +7,20 @@ export const KnowledgeBubble: Component = () => {
   const roundBlur = "blur-sm rounded-full";
 
   return (
-    <div className="relative">
+    <div className="relative -z-10">
       <div className={`${absoluteCenter}`}>
-        <div className={`w-[300px] h-[300px] bg-white ${roundBlur}`} />
+        <div className={`w-[100px] h-[100px] bg-white ${roundBlur}`} />
         <div
           className={`w-[99%] h-[99%] bg-[#86f0f9] ${absoluteCenter} ${roundBlur}`}
         />
         <div
           className={`w-[40%] h-[40%] bg-white ${absoluteCenter} ${roundBlur}`}
         />
+
+        {/* NOTE: tailwindでの動的クラス許容は非推奨＆複雑なスタイルのため、sparkle要素のスタイルに関してはscssで適用する */}
+        {[...new Array(20)].map((_, index) => (
+          <div key={index} className={styles[`sparkle-${index}`]} />
+        ))}
       </div>
     </div>
   );
